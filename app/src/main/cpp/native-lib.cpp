6e18ae36e9741c6b7a5c58322f690c3351252cb5
artem <qzwsecrftb@gmail.com>
2023-06-26 14:45:12 +0300

    init

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_jupiter_tusaandroid_MainActivity_stringFromJNI(
+Java_com_jupiter_tusa_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
